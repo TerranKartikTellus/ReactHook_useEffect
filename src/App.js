a0a1228c0574e6c083dcb9b71f55335a7e3677cc
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2021-10-22 10:40:58 +0530

    REACT HOOK ,UseEffect added: mouse location info

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import { useEffect, useState } from "react";
 export default function App() {
   const [text, setText] = useState("");
   const [num, setNum] = useState(0);
+  const [{ x, y }, setxyz] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     setNum(text.length);
   }, [text]);
+
+  useEffect(() => {
+    const send = (e) => {
+      setxyz({
+        x: e.screenX,
+        y: e.screenY
+      });
+    };
+    window.addEventListener("mousemove", send);
+  }, [x, y]);
 
   return (
     <div className="App">
@@ -21,9 +32,15 @@ export default function App() {
       <h3>You Entered: {text}</h3>
       <h4>Number of alphabets: {num}</h4>
       <p>
-        useEffect( ()=>{} , [var1,var2...] ) gets called eachtime value of any
+        useEffect( ()={} , [var1,var2...] ) gets called eachtime value of any
         element in array changes.
       </p>
+      <br />
+      <br />
+      <h1>Applications : </h1>
+      <h2>
+        Mouse x:{x} , y:{y}{" "}
+      </h2>
     </div>
   );
 }
